8dddd9965af32a612a813093919ef6e8c8784002
wangqi01 <13693607080@163.com>
2024-10-30 16:40:49 +0800

    feat: 暴露方法，html中可调用

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Tarrency 760216236@qq.com
  * @Date: 2024-10-21 16:34:58
- * @LastEditors: Tarrency 760216236@qq.com
- * @LastEditTime: 2024-10-23 23:55:02
+ * @LastEditors: wangqi01 13693607080@163.com
+ * @LastEditTime: 2024-10-30 16:07:30
  * @FilePath: /vanilla-demo/src/js/main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -26,21 +26,28 @@ class ViewerWebComponent extends HTMLElement {
   }
 
   init() {
-    const template = document.createElement('template')
-    template.setAttribute('id', 'viewer-id')
-    // template.innerHTML = TEMPLATE
-    const imageArr = this.getAttribute('images')
-    template.innerHTML = this.imageCount(eval(imageArr))
-
-    document.body.appendChild(template)
-
-    const content = template.content.cloneNode(true); // 克隆一份
-    this.appendChild(content);
-
-    const options = this.getAttribute('options')
-
-    const viewer = new Viewer(document.getElementById('images'), eval(`( ${options} )`));
+    // setTimeout(() => {
+      var shadow = this.attachShadow( { mode: 'open' } );
+      const template = document.createElement('template')
+      // template.setAttribute('id', 'viewer-id')
+      // template.innerHTML = TEMPLATE
+      const imageArr = this.getAttribute('images')
+      template.innerHTML = this.imageCount(eval(imageArr))
+      document.body.appendChild(template)
+  
+      const content = template.content.cloneNode(true); // 克隆一份
+      // this.appendChild(content)
+      shadow.appendChild(content)
+  
+      const options = this.getAttribute('options')
+  
+      const viewer = new Viewer(this.shadowRoot.getElementById('ul-images'), eval(`( ${options} )`));
+      this.viewer = viewer
+      // viewer.show()
+    // })
   }
 }
 window.customElements.define('viewer-webcomponent', ViewerWebComponent);
-export default ViewerWebComponent;
+const viewerElement = document.querySelector('viewer-webcomponent')
+const viewer = viewerElement.viewer
+export default viewer
--- src/js/template.js
+++ src/js/template.js
@@ -1,17 +1,17 @@
 /*
  * @Author: Tarrency 760216236@qq.com
  * @Date: 2024-10-21 17:46:08
- * @LastEditors: Tarrency 760216236@qq.com
- * @LastEditTime: 2024-10-23 23:55:26
+ * @LastEditors: wangqi01 13693607080@163.com
+ * @LastEditTime: 2024-10-30 11:15:00
  * @FilePath: /vanilla-demo/src/js/template.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
 // const innerTemplate =
 //   '<ul id="images">' + '<li><img src="src/assets/tibet-1.jpg"></li>' + '</ul>'
-  const innerTemplate =
-  '<ul id="images">' + '</ul>'
-
+const innerTemplate =
+  // '<div id="viewer-div">'+ '<ul id="ul-images">' + '<slot name="slotName">这是template中的slot</slot>'+ '</ul>' + '</div>'
+  '<slot name="slotName">这是template的slot</slot>' + '<ul id="ul-images">' + '</ul>'
 //   const innerTemplate =
 //     '<ul id="images">'
 //     + '<li><img src="src/assets/tibet-1.jpg"></li>'
